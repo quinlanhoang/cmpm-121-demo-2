9c7805fc84784eb48e8c7b6b9c388f1f1c5f4360
Quinlan <qbhoang@ucsc.edu>
2024-10-23 08:52:11 -0700

    Step 1 Complete

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,5 @@ const APP_NAME = "Sticker Sketchpad";
 const app = document.querySelector<HTMLDivElement>("#app")!;
 
 document.title = APP_NAME;
-app.innerHTML = APP_NAME;
+app.innerHTML = `<h1>${APP_NAME}</h1>
+<canvas id="drawingCanvas" width="256" height="256"></canvas>`;
